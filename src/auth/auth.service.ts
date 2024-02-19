2d4f076b4800cb869f9cb399cf570e1e38ce69df
Bruno Pascaretta Guerra <pascaretta5@gmail.com>
2024-02-19 23:30:40 +0000

    Comitt test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable({})
 export class AuthService {
-    login() {}
+    signup() {
+        return {msg: 'I have signed up'};
+    }
 
-    signup() {}
+    signin() {
+        return {msg: 'I have signed up'};
+    }
 }
 
